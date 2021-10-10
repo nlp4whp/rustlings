775306a052b83da1b7e6a309fe3b50626c4d57d2
nlp4whp <nlp4whp@gmail.com>
2021-10-10 22:15:00 +0800

    enums add

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -1,16 +1,19 @@
 // enums1.rs
 // Make me compile! Execute `rustlings hint enums1` for hints!
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 enum Message {
     // TODO: define a few types of messages as used below
+    Quit(String),
+    Echo,
+    Move {x: i32, y: i32},
+    ChangeColor,
 }
 
 fn main() {
-    println!("{:?}", Message::Quit);
+    println!("{:?}", Message::Quit(String::from("hello Enum")));
     println!("{:?}", Message::Echo);
-    println!("{:?}", Message::Move);
+    println!("{:?}", Message::Move {x: 1, y: 2});
     println!("{:?}", Message::ChangeColor);
 }
