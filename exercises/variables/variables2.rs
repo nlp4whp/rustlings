d3ca2e09827ece9d137ed186b7738b3928c183c5
nlp4whp <nlp4whp@gmail.com>
2021-10-10 14:36:56 +0800

    variables finished

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -1,11 +1,9 @@
 // variables2.rs
 // Make me compile! Execute the command `rustlings hint variables2` if you want a hint :)
 
-// I AM NOT DONE
-
 fn main() {
-    let x;
-    if x == 10 {
+    let x: f32 = 10.0;
+    if x == 10.0 {
         println!("Ten!");
     } else {
         println!("Not ten!");
