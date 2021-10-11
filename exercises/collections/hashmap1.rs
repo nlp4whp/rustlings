509859f596ed6c76aeb2da8bdde9e89a096dccff
nlp4whp <nlp4whp@gmail.com>
2021-10-11 22:04:10 +0800

    learn 101 vec and hashmap

--- exercises/collections/hashmap1.rs
+++ exercises/collections/hashmap1.rs
@@ -11,17 +11,25 @@
 // Execute the command `rustlings hint hashmap1` if you need
 // hints.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new(); // TODO: declare your hash map here.
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket here.
+    let fruits = "apple orange pear onion";
+
+    for word in fruits.split_whitespace() {
+        let cnt = basket.entry(String::from(word)).or_insert(1);
+        *cnt += 1;
+    }
+
+    println!("\t DEBUG:: basket = {:?}", basket);
+
 
     basket
 }
