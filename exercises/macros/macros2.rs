681b5bd53ed082834ce19318528d3c703da6cc58
nlp4whp <nlp4whp@gmail.com>
2021-10-16 17:20:02 +0800

    learn 101 macros

--- exercises/macros/macros2.rs
+++ exercises/macros/macros2.rs
@@ -1,12 +1,13 @@
 // macros2.rs
 // Make me compile! Execute `rustlings hint macros2` for hints :)
 
-// I AM NOT DONE
 
 fn main() {
     my_macro!();
 }
 
+/* 表示该宏可被外部crate使用: */
+#[macro_export]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -2,8 +2,8 @@
 // Make me compile, without taking the macro out of the module!
 // Execute `rustlings hint macros3` for hints :)
 
-// I AM NOT DONE
-
+/* 外部crate声明的宏需要用注解: */
+#[macro_use]
 mod macros {
     macro_rules! my_macro {
         () => {
